7743e3fe388b73f5335e548e0f5393e227809295
nikitaGorin <nikitagn9@gmail.com>
2018-10-18 18:35:47 +0300

    created config of the dream(i hope)

---
+++ src/Containers/App/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classNames from 'classnames/bind';
+import styles from '../../styles/main.scss';
+import exampleStyles from '../../styles/expample.scss';
+
+const mainStyles = classNames.bind(styles);
+const examp = classNames.bind(exampleStyles);
+
+const style = mainStyles(
+	'title',
+	'title_bigFont',
+);
+
+const exam = examp(
+	'title_positionCenter',
+);
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className={`${style} ${exam}` }>
+                hello world
+			</div>
+		);
+	}
+}
+
+export default App;
+
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: { main: './src/index.js' },
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[hash].js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true },
+					},
+				],
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							modules: true,
+							localIdentName: '[local]___[hash:base64:5]',
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: { sourceMap: true },
+					},
+				],
+			},
+		],
+
+	},
+
+	devServer: {
+		// contentBase: path.join(__dirname, 'dist'),
+		// compress: true,
+		hot: true,
+		port: 1717,
+	},
+
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].[hash].css',
+			chunkFilename: '[id].[hash].css',
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+};
